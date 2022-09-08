6dd93f4b63f8c9cf762509c47186e98e839973d0
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-09-08 11:57:26 +0200

    improve example lib, split files, dispose controller after useage, better example with width and height

---
+++ example/lib/home.dart
@@ -0,0 +1,182 @@
+import 'dart:developer';
+
+import 'package:example/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:signature/signature.dart';
+import 'dart:typed_data';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // initialize the signature controller
+  final SignatureController _controller = SignatureController(
+    penStrokeWidth: 1,
+    penColor: Colors.red,
+    exportBackgroundColor: Colors.blue,
+    exportPenColor: Colors.black,
+    onDrawStart: () => log('onDrawStart called!'),
+    onDrawEnd: () => log('onDrawEnd called!'),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() => log('Value changed'));
+  }
+
+  @override
+  void dispose() {
+    // IMPORTANT to dispose of the controller
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> exportImage(BuildContext context) async {
+    if (_controller.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('No content'),
+        ),
+      );
+      return;
+    }
+
+    final Uint8List? data =
+        await _controller.toPngBytes(height: 1000, width: 1000);
+    if (data == null) {
+      return;
+    }
+
+    if (!mounted) return;
+
+    await push(
+      context,
+      Scaffold(
+        appBar: AppBar(
+          title: const Text('PNG Image'),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.grey[300],
+            child: Image.memory(data),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> exportSVG(BuildContext context) async {
+    if (_controller.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('No content'),
+        ),
+      );
+      return;
+    }
+
+    final SvgPicture data = _controller.toSVG()!;
+
+    if (!mounted) return;
+
+    await push(
+      context,
+      Scaffold(
+        appBar: AppBar(
+          title: const Text('SVG Image'),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.grey[300],
+            child: data,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Signature Demo'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          const SizedBox(
+            height: 300,
+            child: Center(
+              child: Text('Big container to test scrolling issues'),
+            ),
+          ),
+          //SIGNATURE CANVAS
+          Signature(
+            controller: _controller,
+            height: 300,
+            backgroundColor: Colors.grey[300]!,
+          ),
+          //OK AND CLEAR BUTTONS
+          Container(
+            decoration: const BoxDecoration(color: Colors.black),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                //SHOW EXPORTED IMAGE IN NEW ROUTE
+                IconButton(
+                  icon: const Icon(Icons.image),
+                  color: Colors.blue,
+                  onPressed: () => exportImage(context),
+                  tooltip: 'Export Image',
+                ),
+                IconButton(
+                  icon: const Icon(Icons.share),
+                  color: Colors.blue,
+                  onPressed: () => exportSVG(context),
+                  tooltip: 'Export SVG',
+                ),
+                IconButton(
+                  icon: const Icon(Icons.undo),
+                  color: Colors.blue,
+                  onPressed: () {
+                    setState(() => _controller.undo());
+                  },
+                  tooltip: 'Undo',
+                ),
+                IconButton(
+                  icon: const Icon(Icons.redo),
+                  color: Colors.blue,
+                  onPressed: () {
+                    setState(() => _controller.redo());
+                  },
+                  tooltip: 'Redo',
+                ),
+                //CLEAR CANVAS
+                IconButton(
+                  icon: const Icon(Icons.clear),
+                  color: Colors.blue,
+                  onPressed: () {
+                    setState(() => _controller.clear());
+                  },
+                  tooltip: 'Clear',
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 300,
+            child: Center(
+              child: Text('Big container to test scrolling issues'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,157 +1,27 @@
-import 'dart:typed_data';
-
+import 'package:example/home.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:signature/signature.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
-/// example widget showing how to use signature widget
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  _MyAppState createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final SignatureController _controller = SignatureController(
-    penStrokeWidth: 1,
-    penColor: Colors.red,
-    exportBackgroundColor: Colors.blue,
-    exportPenColor: Colors.black,
-    onDrawStart: () => print('onDrawStart called!'),
-    onDrawEnd: () => print('onDrawEnd called!'),
-  );
-
-  @override
-  void initState() {
-    super.initState();
-    _controller.addListener(() => print('Value changed'));
-  }
-
-  Future<void> exportImage(BuildContext context) async {
-    if (_controller.isEmpty) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(const SnackBar(content: Text('No content')));
-      return;
-    }
-
-    final Uint8List? data = await _controller.toPngBytes();
-    if (data == null) {
-      return;
-    }
-
-    await Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          return Scaffold(
-            appBar: AppBar(),
-            body: Center(
-              child: Container(
-                color: Colors.grey[300],
-                child: Image.memory(data),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  Future<void> exportSVG(BuildContext context) async {
-    if (_controller.isEmpty) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(const SnackBar(content: Text('No content')));
-      return;
-    }
-
-    final SvgPicture data = _controller.toSVG()!;
-
-    await Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          return Scaffold(
-            appBar: AppBar(),
-            body: Center(
-              child: Container(color: Colors.grey[300], child: data),
-            ),
-          );
-        },
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Builder(
-        builder: (BuildContext context) => Scaffold(
-          body: ListView(
-            children: <Widget>[
-              Container(
-                height: 300,
-                child: const Center(
-                  child: Text('Big container to test scrolling issues'),
-                ),
-              ),
-              //SIGNATURE CANVAS
-              Signature(
-                controller: _controller,
-                height: 300,
-                backgroundColor: Colors.lightBlueAccent,
-              ),
-              //OK AND CLEAR BUTTONS
-              Container(
-                decoration: const BoxDecoration(color: Colors.black),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    //SHOW EXPORTED IMAGE IN NEW ROUTE
-                    IconButton(
-                      icon: const Icon(Icons.image),
-                      color: Colors.blue,
-                      onPressed: () => exportImage(context),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.polyline),
-                      color: Colors.blue,
-                      onPressed: () => exportSVG(context),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.undo),
-                      color: Colors.blue,
-                      onPressed: () {
-                        setState(() => _controller.undo());
-                      },
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.redo),
-                      color: Colors.blue,
-                      onPressed: () {
-                        setState(() => _controller.redo());
-                      },
-                    ),
-                    //CLEAR CANVAS
-                    IconButton(
-                      icon: const Icon(Icons.clear),
-                      color: Colors.blue,
-                      onPressed: () {
-                        setState(() => _controller.clear());
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: 300,
-                child: const Center(
-                  child: Text('Big container to test scrolling issues'),
-                ),
-              ),
-            ],
-          ),
-        ),
+      title: 'Signature Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      darkTheme: ThemeData.dark().copyWith(
+        primaryColor: Colors.blue,
+      ),
+      home: const Home(),
     );
   }
 }
